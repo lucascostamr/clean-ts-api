ae4ee89ba73f92fda8f8c5e747ed1c481880f6e9
dreamagici4n <lucascostamr812@gmail.com>
2024-01-12 10:00:18 -0300

    chore: add jest to lint-staged

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-  roots: ['<rootDir/src>'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
   testEnvironment: "jest-environment-node",
---
+++ src/controller.spec.ts
@@ -0,0 +1,5 @@
+describe('teste', () => {
+    test('', () => {
+        expect(1).toBe(1);
+    });
+});
