7f88698ed595215f3f0a26d66886deb551c3d1fe
dreamagici4n <lucascostamr812@gmail.com>
2024-01-15 10:36:31 -0300

    feat: ensure SignUpController return error if password confirmation fails

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -17,6 +17,8 @@ export class SignUpController implements Controller {
         if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       };
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
+
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
 
       if (!isValid) {
