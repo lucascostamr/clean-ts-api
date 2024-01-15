658de229ea168bdc522872be13f6c4bbaead0492
dreamagici4n <lucascostamr812@gmail.com>
2024-01-15 10:04:01 -0300

    test: move all protocols to a sigle file

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signUp'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
-import { type EmailValidator } from '../protocols/email-validator'
+import { type EmailValidator } from '../protocols'
 
 interface SutTypes {
   sut: SignUpController
