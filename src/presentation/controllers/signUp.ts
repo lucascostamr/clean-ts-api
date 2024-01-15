33809c47bddc3ee52a01a4b62dcfa795033d7cb9
dreamagici4n <lucascostamr812@gmail.com>
2024-01-15 09:52:56 -0300

    refactor: create helper for serverError and apply it to SignUpController

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -2,9 +2,8 @@ import type { HttpResponse, HttpRequest } from '../protocols/http'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
 import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -32,10 +31,7 @@ export class SignUpController implements Controller {
         body: { message: 'Successfull' }
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
