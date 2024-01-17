7b26c939587826e9eeb3fb6cdf5a99174fd3f819
dreamagici4n <lucascostamr812@gmail.com>
2024-01-17 10:30:36 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ mySrc/presentation/controllers/signup/signUp.ts
@@ -0,0 +1,10 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+
+export class SignUpController {
+  handle (httpRequest: any): any {
+    return {
+      statusCode: 400,
+      body: new MissingParamError('name')
+    }
+  }
+}
