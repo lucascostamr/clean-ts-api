9c66d4432ee42cc4ccefdde23f91c3e88bf41f20
dreamagici4n <lucascostamr812@gmail.com>
2024-01-31 16:58:08 -0300

    feat: ensure LogControllerDecorator call controller handle with correct values

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,21 @@
+import { type Controller, type HttpRequest, type HttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpRequest)
+    return await new Promise(resolve => {
+      resolve({
+        statusCode: 200,
+        body: {
+          name: 'Lucas'
+        }
+      })
+    })
+  }
+}
