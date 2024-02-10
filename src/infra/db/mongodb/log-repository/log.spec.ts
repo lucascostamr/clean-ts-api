5c8424454cbd506b89f9b431b0e729114bf0316a
dreamagici4n <lucascostamr812@gmail.com>
2024-02-10 10:34:08 -0300

    test: ensure LogMongoRepository creates error on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,28 @@
+import { type Collection } from 'mongodb'
+
+import { MongoHelper } from '../helper/mongodb-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Error Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const cont = await errorCollection.countDocuments()
+    expect(cont).toBe(1)
+  })
+})
