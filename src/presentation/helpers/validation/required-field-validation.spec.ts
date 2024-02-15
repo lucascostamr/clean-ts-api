ef9e27b397d87e9371c8575d6cce48ce33be8f7a
dreamagici4n <lucascostamr812@gmail.com>
2024-02-15 10:59:59 -0300

    test: ensure RequiredFieldValidation return null on success

--- src/presentation/helpers/validation/required-field-validation.spec.ts
+++ src/presentation/helpers/validation/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('Required Fields Validation', () => {
     const response = sut.validate({})
     expect(response).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return null on success', () => {
+    const sut = new RequiredFieldValidation('email')
+    const response = sut.validate({ email: 'any_email' })
+    expect(response).toBe(null)
+  })
 })
