958882b9acd198ef2296b6d52124e077909d9559
dreamagici4n <lucascostamr812@gmail.com>
2024-02-15 10:53:40 -0300

    test: ensure ComapareFieldsValidation return an InvalidParamError if validation fails

---
+++ src/presentation/helpers/validation/compare-fields-validation.spec.ts
@@ -0,0 +1,14 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+describe('Compare Fields Validation', () => {
+  test('Should return InvalidParamError if validation fails', () => {
+    const sut = new CompareFieldsValidation('password', 'passwordConfirmation')
+    const input = {
+      password: 'any_password',
+      passwordConfirmation: 'invalid_password'
+    }
+    const response = sut.validate(input)
+    expect(response).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+})
