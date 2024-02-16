b52325e7fe27fb8e81cf5ec7b9f6685af18e908e
dreamagici4n <lucascostamr812@gmail.com>
2024-02-16 10:21:17 -0300

    refactor: add AthenticationModel

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
-import { type HttpRequest, type Authentication, type Validation } from './login-protocols'
+import { type HttpRequest, type Authentication, type Validation, type AuthenticationModel } from './login-protocols'
 import { LoginController } from './login'
 
 const makeValidation = (): Validation => {
@@ -14,7 +14,7 @@ const makeValidation = (): Validation => {
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (email: string, password: string): Promise<string> {
+    async auth (authentication: AuthenticationModel): Promise<string> {
       return await new Promise(resolve => { resolve('any_token') })
     }
   }
@@ -66,7 +66,10 @@ describe('Login Controller', () => {
     const { sut, authenticationStub } = makeSut()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
     await sut.handle(makeFakeRequest())
-    expect(authSpy).toHaveBeenCalledWith('any_email', 'any_password')
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email',
+      password: 'any_password'
+    })
   })
 
   test('Should return 401 if Authentication fails', async () => {
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -15,7 +15,10 @@ export class LoginController implements Controller {
       const validationError = this.validation.validate(httpRequest.body)
       if (validationError) return badRequest(validationError)
       const { email, password } = httpRequest.body
-      const accessToken = await this.authentication.auth(email, password)
+      const accessToken = await this.authentication.auth({
+        email,
+        password
+      })
       if (!accessToken) return unauthorized()
       return ok({ accessToken })
     } catch (error) {
