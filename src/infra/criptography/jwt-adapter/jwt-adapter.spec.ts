5c002ad958c7c9bad9a79e398a0c766d25529137
dreamagici4n <lucascostamr812@gmail.com>
2024-02-20 09:32:53 -0300

    test: ensure JwtAdapter calls jwt sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter'
+
+describe('JWT Adapter', () => {
+  test('Should call jwt sign with correct values', async () => {
+    const sut = new JwtAdapter('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_value')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+  })
+})
