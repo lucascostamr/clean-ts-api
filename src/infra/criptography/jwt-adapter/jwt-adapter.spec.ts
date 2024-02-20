b5e89435838f349ea83619ba6da0469821c7381b
DreaMagici4n <lucascostamr812@gmail.com>
2024-02-20 14:43:12 -0300

    feat: ensure JwtAdapter throws if jwt sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,24 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'token'
+  }
+}))
+
 describe('JWT Adapter', () => {
   test('Should call jwt sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_value')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+  })
+
+  test('Should throw if jwt sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const response = sut.encrypt('any_value')
+    await expect(response).rejects.toThrow(new Error())
   })
 })
