0573c4e7dd8548ea9a4707c9b0e48d2c879112cf
DreaMagici4n <lucascostamr812@gmail.com>
2024-02-20 15:31:41 -0300

    feat: ensure AccountMongoRepository returns null if loadByEmail fails

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -15,6 +15,7 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadByEmail (email: string): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const accountResult = await accountCollection.findOne({ email })
+    if (!accountResult) return null
     return MongoHelper.map(accountResult)
   }
 }
