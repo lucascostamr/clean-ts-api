dc33666881db005b195dcf6da5cb13dc1105e121
dreamagici4n <lucascostamr812@gmail.com>
2024-02-22 10:22:59 -0300

    test: ensure login route returns 401 if no account is found

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 if no account found', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lucascostamr812@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
