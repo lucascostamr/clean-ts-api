98b2b437fd1c5afc67c58bbc24bc6bef2bf47fdd
dreamagici4n <lucascostamr812@gmail.com>
2024-02-22 10:19:57 -0300

    test: add login route

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongodb-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+describe('Login Routes', () => {
+  let accountCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect((process.env.MONGO_URI ?? ''))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Lucas',
+          email: 'lucascostamr812@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Lucas',
+        email: 'lucascostamr812@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lucascostamr812@gmail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
+  })
+})
