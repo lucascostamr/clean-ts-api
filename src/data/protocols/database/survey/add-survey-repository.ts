9099237ae88655467adb5ca1556db3ddd5f92c2d
dreamagici4n <lucascostamr812@gmail.com>
2024-03-05 17:22:24 -0300

    feat: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/protocols/database/survey/add-survey-repository.ts
@@ -0,0 +1,5 @@
+import { type AddSurveyModel } from '../../../../domain/models/add-survey'
+
+export interface AddSurveyRepository {
+  add: (survey: AddSurveyModel) => Promise<void>
+}
---
+++ src/data/usercases/addsurvey/db-add-survey.ts
@@ -0,0 +1,11 @@
+import { type AddSurveyModel, type AddSurvey, type AddSurveyRepository } from './db-add-survey-protocols'
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (
+    private readonly addSurveyRepository: AddSurveyRepository
+  ) {}
+
+  async add (survey: AddSurveyModel): Promise<void> {
+    await this.addSurveyRepository.add(survey)
+  }
+}
