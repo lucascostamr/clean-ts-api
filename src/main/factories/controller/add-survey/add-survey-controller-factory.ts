be5f9a4e7ea11a9787f465efd2ab8e804cc3e998
dreamagici4n <lucascostamr812@gmail.com>
2024-03-05 18:12:26 -0300

    feat: ensure AddSurveyValidation facotry calls ValidationComposite with correct validations

---
+++ src/main/factories/controller/add-survey/add-survey-controller-factory.ts
@@ -0,0 +1,8 @@
+import { AddSurveyController } from '../../../../presentation/controllers/survey/add-survey/add-survey-controller'
+import { type Controller } from '../../../../presentation/protocols'
+import { makeDbAddSurvey } from '../../usecases/addsurvey/db-add-survey'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+export const makeAddSurveyController = (): Controller => {
+  return new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
+}
---
+++ src/main/factories/controller/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,10 @@
+import { type Validation } from '../../../../validation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validations'
+
+export const makeAddSurveyValidation = (): Validation => {
+  const validations: Validation[] = []
+  for (const field of ['question', 'answers']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
