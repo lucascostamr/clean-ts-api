dc27fbbd7f2ffbdbdc5f8be1addd2f82caa95b7b
dreamagici4n <lucascostamr812@gmail.com>
2024-03-05 17:48:07 -0300

    feat: ensure SurveyMongoRepository add survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,9 @@
+import { type AddSurveyModel, type AddSurveyRepository } from '../../../../data/usercases/addsurvey/db-add-survey-protocols'
+import { MongoHelper } from '../helper/mongodb-helper'
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (survey: AddSurveyModel): Promise<void> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.insertOne(survey)
+  }
+}
