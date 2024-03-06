ba957ead5fcdb34aae672e76f6e0630f5ae04027
dreamagici4n <lucascostamr812@gmail.com>
2024-03-06 10:36:11 -0300

    test: add AddSurvey route

---
+++ src/main/routes/add-survey-routes.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongodb-helper'
+import { type Collection } from 'mongodb'
+
+describe('Add Survey Routes', () => {
+  let accountCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect((process.env.MONGO_URI ?? ''))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('surveys')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return 204 on surveys', async () => {
+    await request(app)
+      .post('/api/surveys')
+      .send({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image_url',
+            answer: 'any_answer'
+          }
+        ]
+      })
+      .expect(204)
+  })
+})
