a6b7cfd6af4c39a2c96fa24cc7f6f45e7d7b3b1e
dreamagici4n <lucascostamr812@gmail.com>
2024-03-06 16:42:08 -0300

    test: ensure AuthMiddleware returns 403 if no x-access-token is provided

---
+++ src/presentation/middlewares/auth-middlware.spec.ts
@@ -0,0 +1,13 @@
+import { AuthMiddleware } from './auth-middleware'
+import { forbidden } from '../helpers/http/http-helper'
+const makeSut = (): AuthMiddleware => {
+  return new AuthMiddleware()
+}
+
+describe('Authentication Middleware', () => {
+  test('Should return 403 if no x-access-token is provided', async () => {
+    const sut = makeSut()
+    const response = await sut.handle({})
+    expect(response).toEqual(forbidden())
+  })
+})
