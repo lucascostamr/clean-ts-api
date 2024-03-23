b4bc4362e33313689ca2635546e0d8c844f005da
lucascostamr <lucascostamr812@gmail.com>
2024-03-23 14:51:30 -0300

    feat: ensure JwtAdapter calls jwt verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,17 @@
+import { type Decrypter } from '../../../data/protocols/criptograph/decrypter'
 import { type Encrypter } from '../../../data/protocols/criptograph/encrypter'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (value: string): Promise<string> {
     const token = jwt.sign({ id: value }, this.secret)
     return await new Promise(resolve => { resolve(token) })
+  }
+
+  async decrypt (value: string): Promise<string | null> {
+    const response = jwt.verify(value, this.secret)
+    return await new Promise(resolve => { resolve(response as string) })
   }
 }
