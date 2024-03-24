33b658861b21b4a76f920ac2c38f2ca5615c72f0
lucascostamr <lucascostamr812@gmail.com>
2024-03-24 12:06:58 -0300

    test: ensure add survey route returns 204 on addSurvey success

--- src/main/routes/add-survey-routes.test.ts
+++ src/main/routes/add-survey-routes.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helper/mongodb-helper'
 import { type Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 describe('Add Survey Routes', () => {
   let accountCollection: Collection
@@ -15,7 +17,7 @@ describe('Add Survey Routes', () => {
   })
 
   beforeEach(async () => {
-    accountCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -32,5 +34,30 @@ describe('Add Survey Routes', () => {
         ]
       })
       .expect(403)
+  })
+
+  test('Should return 204 on addSurvey success', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'Lucas',
+      email: 'lucascostamr812@gmail.com',
+      password: '1234',
+      role: 'admin'
+    })
+    const id = res.insertedId.toString()
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({ _id: res.insertedId }, { $set: { accessToken } })
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image_url',
+            answer: 'any_answer'
+          }
+        ]
+      })
+      .expect(204)
   })
 })
