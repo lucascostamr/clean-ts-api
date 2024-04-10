7ffaf6f1242fa5e9835a5111c1ff640d7829f8aa
lucascostamr <lucascostamr812@gmail.com>
2024-04-10 10:37:37 -0300

    test: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct value

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,47 @@
+import mockDate from 'mockdate'
+import { type SurveyModel } from '../../../domain/models/survey'
+import { type LoadSurveyByIdRepository } from '../../protocols/database/survey/load-survey-by-id-repository'
+import { DbLoadSurveyById } from './db-load-survey-by-id'
+
+const makeLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
+  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+    async loadById (surveyId: string): Promise<SurveyModel> {
+      return await new Promise(resolve => { resolve(makeFakeSurvey()) })
+    }
+  }
+  return new LoadSurveyByIdRepositoryStub()
+}
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{ image: 'any_image', answer: 'any_answer' }],
+  date: new Date()
+})
+
+type SutTypes = {
+  sut: DbLoadSurveyById
+  loadSurveyByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdRepositoryStub = makeLoadSurveyByIdRepository()
+  const sut = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+  return {
+    sut,
+    loadSurveyByIdRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyById', () => {
+  beforeAll(() => { mockDate.set(new Date()) })
+
+  afterAll(() => { mockDate.reset() })
+
+  test('Should calls LoadSurveyByIdRepository with correct value', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+    await sut.loadById('any_id')
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
