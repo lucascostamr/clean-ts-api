ea3b0e74d8b6f3d5c7a31fcda3ae5076414b0604
lucascostamr <lucascostamr812@gmail.com>
2024-04-10 10:05:04 -0300

    feat: add SaveSurveyResult usecase

---
+++ src/domain/usecases/save-survey-result.ts
@@ -0,0 +1,7 @@
+import { type SurveyResultModel } from '../models/survey-result'
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>
+
+export interface SaveSurveyResult {
+  save: (survey: SaveSurveyResultModel) => Promise<SurveyResultModel>
+}
