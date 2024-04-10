34b17401c1fe1b4d0e544b43fef9283c672c582b
lucascostamr <lucascostamr812@gmail.com>
2024-04-10 10:37:20 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct value

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,17 @@
+import { type SurveyModel } from '../../../domain/models/survey'
+import { type LoadSurveyById } from '../../../domain/usecases/load-survey-by-id'
+import { type LoadSurveyByIdRepository } from '../../protocols/database/survey/load-survey-by-id-repository'
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor (private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
+
+  async loadById (surveyId: string): Promise<SurveyModel> {
+    await this.loadSurveyByIdRepository.loadById(surveyId)
+    return {
+      id: 'any_id',
+      question: 'any_question',
+      answers: [{ image: 'any_image', answer: 'any_answer' }],
+      date: new Date()
+    }
+  }
+}
